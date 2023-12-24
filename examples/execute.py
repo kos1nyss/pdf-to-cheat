5c8d2a3be66460133a121c283072349997626d4b
Konstantin Kondrashov <kondrashovkostyau@gmail.com>
2023-12-24 16:41:20 +0200

    Примеры использования метода execute()

---
+++ examples/execute.py
@@ -0,0 +1,9 @@
+from converter import Converter
+
+conv = Converter()
+
+# 1) Вариант с одним файлом, pdf превращается в docx
+conv.execute('one.docx', pdf_path='example.pdf')
+
+# 2) Вариант с папкой, берутся все файлы из папки и собираются в один docx
+conv.execute('many.docx', folder_with_pdfs_path='/example-folder')
