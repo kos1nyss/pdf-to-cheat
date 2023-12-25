3212e31c1ffe4f45362b4cfdabda00973988913b
Konstantin Kondrashov <kondrashovkostyau@gmail.com>
2023-12-25 11:02:01 +0200

    Добавлен pdf для примера побольше

--- examples/execute.py
+++ examples/execute.py
@@ -3,7 +3,7 @@ from converter import Converter
 conv = Converter()
 
 # 1) Вариант с одним файлом, pdf превращается в docx
-conv.execute('one.docx', pdf_path='pdf/example.pdf')
+conv.execute('one.docx', pdf_path='pdf/big-example.pdf')
 
 # 2) Вариант с папкой, берутся все файлы из папки и собираются в один docx
 conv.execute('many.docx', folder_with_pdfs_path='/pdf')
